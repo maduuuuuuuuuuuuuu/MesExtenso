89f3d3418a7cffc61fd1b9919b9b5c60abc46e44
madu <maduamaral2006@gmail.com>
2023-07-29 16:10:16 -0300

    exercicio resolvido

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿double mes;
+Console.WriteLine("digite o número do mês");
+mes = Convert.ToDouble(Console.ReadLine()!);
+if ( mes == 1){
+    Console.WriteLine($"{mes} = janeiro");
+}
+if ( mes == 2){
+    Console.WriteLine($"{mes} = fevereiro");
+}
+if ( mes == 3){
+    Console.WriteLine($"{mes} = março");
+}
+if ( mes == 4){
+    Console.WriteLine($"{mes} = abril");
+}
+if ( mes == 5){
+    Console.WriteLine($"{mes} = maio");
+}
+if ( mes == 6){
+    Console.WriteLine($"{mes} = junho");
+}
+if ( mes == 7){
+    Console.WriteLine($"{mes} = julho");
+}
+if ( mes == 8){
+    Console.WriteLine($"{mes} = agosto");
+}
+if ( mes == 9){
+    Console.WriteLine($"{mes} = setembro");
+}
+if ( mes == 10){
+    Console.WriteLine($"{mes} = outubro");
+}
+if ( mes == 11){
+    Console.WriteLine($"{mes} = novembro");
+}
+if ( mes == 12){
+    Console.WriteLine($"{mes} = dezembro");
+}
